0fff57ef54059da7f8ac47324e16718d16c9655d
wangjiangyu-creator <wangjiangyu@gmail.com>
2026-06-26 19:51:20 +0800

    Use custom domain root path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://wangjiangyu-creator.github.io',
-  base: '/chuhai',
+  site: 'https://chuhai.eastlaw.wang',
+  base: '/',
   output: 'static',
   build: {
     assets: 'assets'
